3d341f1c1e2b557d82792213c80300f6867b9ec0
Linweizhang27 <154345163+Linweizhang27@users.noreply.github.com>
2026-04-01 22:04:06 -0400

    I think the function is correct now

--- Algorithm.py
+++ Algorithm.py
@@ -1,17 +1,34 @@
 def find_max_subsequence(val, A, B):
-    M = []
-    
-    for i in range(len(A)):
+
+    M = [[0] * (len(B) + 1) for _ in range(len(A) + 1)]
+
+    '''for i in range(len(A)+1):
         M[i][0] = 0
     
-    for j in range(len(B)):
-        M[0][j] = 0
+    for j in range(len(B)+1):
+        M[0][j] = 0'''
 
-    for i in range(len(A)):
-        for j in range(len(B)):
-            if (A[i] == B[j]):
-                 M[i][j] = max(M[i-1][j-1]+ val[A[i]], M[i-1][j], M[i][j-1])
+    for i in range(1, len(A)+1):
+        for j in range(1, len(B)+1):
+            if (A[i-1] == B[j-1]): #remember that A and B are still zero-indexed
+                 M[i][j] = max(M[i-1][j-1]+ val[A[i-1]], M[i-1][j], M[i][j-1]) #the val dictionary is also 0-indexed!!
             else:
-                M[i][j] = max
+                M[i][j] = max(M[i][j-1], M[i-1][j])
 
-                 
+    print(M[len(A)][len(B)])
+    return M[len(A)][len(B)]
+
+def main():
+    val = {}
+    val['a'] = 2
+    val['b'] = 4
+    val['c'] = 5
+
+    A = ['a', 'a', 'c', 'b']
+    B = ['c', 'a', 'a', 'b']
+
+    find_max_subsequence(val, A, B)
+
+
+if __name__ == "__main__":
+    main()
